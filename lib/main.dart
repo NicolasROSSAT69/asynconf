10261e6b315d9fefc0c211d42fe7c1e8d8059161
ROSSAT Nicolas <75442011+NicolasROSSAT69@users.noreply.github.com>
2023-08-23 13:43:38 +0200

    Séparation des widgets et ajout navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:asynconf/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Asynconf 2023"),
-        ),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                    "assets/images/logo.svg",
-                  color: Colors.blue,
-                ),
-                const Text(
-                    "Asynconf 2023",
-                  style: TextStyle(
-                    fontSize: 42,
-                    fontFamily: 'Poppins'
-                  ),
-                ),
-                const Text(
-                  "Salon virtuel de l'informatique. Du 27 au 29 octobre 2023",
-                  style: TextStyle(
-                    fontSize: 24
-                  ),
-                  textAlign: TextAlign.center,
-                )
-              ],
-            )
-        )
-      ),
+      home: HomePage(),
     );
   }
 }
