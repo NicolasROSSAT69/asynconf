c08c66f31c66a7d237d7aeb7150cbfc18b659f7e
ROSSAT Nicolas <75442011+NicolasROSSAT69@users.noreply.github.com>
2023-08-27 11:17:06 +0200

    Ajout bdd Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,17 @@ import 'package:asynconf/pages/event_page.dart';
 import 'package:asynconf/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 class MyApp extends StatefulWidget {
