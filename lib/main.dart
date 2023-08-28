25481428913b84b835619514b6ff9735631e01d9
ROSSAT Nicolas <75442011+NicolasROSSAT69@users.noreply.github.com>
2023-08-28 20:53:41 +0200

    Ajout SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:asynconf/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:global_bottom_navigation_bar/global_bottom_navigation_bar.dart';
+
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 import 'firebase_options.dart';
 
@@ -18,6 +21,7 @@ void main() async {
 
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -37,27 +41,54 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     });
   }
 
+  Widget buildSplashScreen() {
+    return FlutterSplashScreen.fadeIn(
+      backgroundColor: Colors.white,
+      onInit: () {
+        debugPrint("On Init");
+      },
+      onEnd: () {
+        debugPrint("On End");
+      },
+      childWidget: SizedBox(
+        height: 200,
+        width: 200,
+        child: SvgPicture.asset(
+          "assets/images/logo.svg",
+          color: Colors.blue,
+        ),
+      ),
+      onAnimationEnd: () => debugPrint("On Fade In End"),
+      defaultNextScreen: buildMainScreen(),
+    );
+  }
+
+  Widget buildMainScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: [
+          const Text("Accueil"),
+          const Text("Planning"),
+          const Text("Formulaire")
+        ][_currentIndex],
+      ),
+      body: ScaffoldGlobalBottomNavigation(
+        listOfChild: const [
+          HomePage(),
+          EventPage(),
+          AddEventPage(),
+        ],
+        listOfBottomNavigationItem: buildBottomNavigationItemList(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: [
-            const Text("Accueil"),
-            const Text("Planning"),
-            const Text("Formulaire")
-          ][_currentIndex],
-        ),
-        body: ScaffoldGlobalBottomNavigation(
-          listOfChild: const [
-            HomePage(),
-            EventPage(),
-            AddEventPage()
-          ],
-          listOfBottomNavigationItem: buildBottomNavigationItemList(),
-        ),
-      ),
+      home: buildSplashScreen(),
     );
+
   }
 
   List<BottomNavigationItem> buildBottomNavigationItemList() => [
